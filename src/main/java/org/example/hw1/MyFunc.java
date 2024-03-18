632597e82318076177e10dc952baffbd3ade6a06
Voronin Egor <vegor4956@gmail.com>
2024-03-19 02:28:45 +0300

    a

--- src/main/java/org/example/hw1/MyFunc.java
+++ src/main/java/org/example/hw1/MyFunc.java
@@ -19,7 +19,9 @@ public class MyFunc implements Function<List<Student>, Map<String,List<Student>>
         studentList.add(new Student("A"));
         studentList.add(new Student("B"));
         studentList.add(new Student("B"));
-        studentList.add(new Student("C"));\
+        studentList.add(new Student("C"));
+        studentList.add(new Student("C"));
+        studentList.add(new Student("C"));
         studentList.add(new Student("A"));
 
         MyFunc myFunc = new MyFunc();
